f091da898617f26bc7de756cb1fdd3c896a94324
sebastianmsz <sevas.molina2004@gmail.com>
2024-01-04 20:36:10 -0500

    Add function that gets the user’s choice
    rock, paper or scissors.

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ function getComputerChoice () {
     let num = Math.floor(Math.random() * 3) + 1;
     switch (num) {
         case 1:
-            return 'Rock';    
+            return 'rock';    
         case 2:
-            return 'Paper';
+            return 'raper';
         case 3:
-            return 'Scissors';
+            return 'scissors';
     }
 }
+
+function getUserChoice () {
+    let userChoice = prompt('Choose rock, paper or scissors');
+    userChoice = String(userChoice);
+    userChoice = userChoice.toLowerCase();
+    while (userChoice !== 'rock' && userChoice !== 'paper' && userChoice !== 'scissors') {
+        alert('Thats not a valid option');
+        userChoice = prompt('Choose rock, paper or scissors');
+    }
+    return userChoice;
+}
+
+//test
+alert (`Computer:${getComputerChoice()} User:${getUserChoice()}`);
 
